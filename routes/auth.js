b26812839931063c3d2110027682445426dfe5b7
Satabrata Paul <satabrata.slg88@gmail.com>
2022-06-29 01:59:23 +0530

    Create basic registration route

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,23 @@
 const router = require("express").Router()
+const User = require('../models/User')
 
-router.get("/", (req, res)=>{
-    res.send({status: 200, message: "Welcome to users route"})
+//REGISTER A USER
+router.post("/register", async (req, res)=>{
+    const body = req.body;
+    const password = body.password;
+    const newUser = User({
+        userName: body.username,
+        email: body.email,
+        password: password,
+    })
+
+    try {
+        await newUser.save();
+        res.status(200).json({message: "User registered successfilly"});
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message: "Server error"})
+    }
 })
 
 module.exports = router
