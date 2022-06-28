6236508c8af1b26f57ef63cc21d5bd84ab3bd3b9
Satabrata Paul <satabrata.slg88@gmail.com>
2022-06-28 22:12:53 +0530

    Create User model schema

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 25,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profilePicture: {
+        type: String,
+        default: ""
+    },
+    coverPicture: {
+        type:String,
+        default: ""
+    },
+    followers: {
+        type: Array,
+        default: []
+    },
+    following: {
+        type: Array,
+        default: []
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+},
+{timestamps: true})
+
+module.exports = mongoose.model('User', userSchema)
