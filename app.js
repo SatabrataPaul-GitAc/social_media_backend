bfefe7bb4ce3139c914d78c1d2f4e24829cb3906
Satabrata Paul <satabrata.slg88@gmail.com>
2022-06-28 20:56:17 +0530

    Basic user route

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require('express');
 require('dotenv').config();
+const helmet = require('helmet');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const userRoute = require('./routes/users')
+
+//Establishing connection to mongodb database
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true}, ()=>{
+    console.log("Connected to the database successfully");
+})
+
 //Creating our application
 const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan());
+
+app.use('/api/users', userRoute)
+
 
 //Starting the express application
 app.listen(process.env.PORT, ()=>{
