1287e61e1afa19bb9781b75370e43d6641563c23
Satabrata Paul <satabrata.slg88@gmail.com>
2022-06-29 10:19:17 +0530

    Check for body parameters

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,15 @@ const User = require('../models/User')
 //REGISTER A USER
 router.post("/register", async (req, res)=>{
     const body = req.body;
+    if (!body.username){
+        res.status(400).json({message: "username is missing"})
+    }
+    if (!body.email){
+        res.status(400).json({message: "Email address is missing"})
+    }
+    if (!body.password){
+        res.status(400).json({message: "Password is missing"})
+    }
     const password = body.password;
     const newUser = User({
         userName: body.username,
